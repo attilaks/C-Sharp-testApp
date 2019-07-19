4bc4f62ba4e26afd7fc9cac87fce078b9b84421c
attilaks <47320892+attilaks@users.noreply.github.com>
2019-07-20 02:16:46 +0300

    Добавьте файлы проекта.

---
+++ Test app/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Test_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.ApplyFilter = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.openFile.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openFile.Location = new System.Drawing.Point(725, 11);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(94, 44);
+            this.openFile.TabIndex = 0;
+            this.openFile.Text = "Открыть файл";
+            this.openFile.UseVisualStyleBackColor = false;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.AcceptsReturn = true;
+            this.textBoxMain.AllowDrop = true;
+            this.textBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMain.Location = new System.Drawing.Point(22, 128);
+            this.textBoxMain.Multiline = true;
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxMain.Size = new System.Drawing.Size(795, 372);
+            this.textBoxMain.TabIndex = 2;
+            this.textBoxMain.WordWrap = false;
+            this.textBoxMain.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxMain_DragDrop);
+            this.textBoxMain.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxMain_DragEnter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "(*.txt)|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "(*.txt)|*.txt";
+            // 
+            // saveFile
+            // 
+            this.saveFile.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.saveFile.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveFile.Location = new System.Drawing.Point(725, 78);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(94, 44);
+            this.saveFile.TabIndex = 4;
+            this.saveFile.Text = "Сохранить";
+            this.saveFile.UseVisualStyleBackColor = false;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // ApplyFilter
+            // 
+            this.ApplyFilter.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ApplyFilter.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ApplyFilter.Location = new System.Drawing.Point(22, 11);
+            this.ApplyFilter.Name = "ApplyFilter";
+            this.ApplyFilter.Size = new System.Drawing.Size(94, 43);
+            this.ApplyFilter.TabIndex = 5;
+            this.ApplyFilter.Text = "Применить фильтр";
+            this.ApplyFilter.UseVisualStyleBackColor = false;
+            this.ApplyFilter.Click += new System.EventHandler(this.ApplyFilter_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 16;
+            this.comboBox1.Items.AddRange(new object[] {
+            "и",
+            "или"});
+            this.comboBox1.Location = new System.Drawing.Point(151, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(52, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "и",
+            "или"});
+            this.comboBox2.Location = new System.Drawing.Point(151, 78);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(52, 24);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "и",
+            "или"});
+            this.comboBox3.Location = new System.Drawing.Point(434, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(52, 24);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "и",
+            "или"});
+            this.comboBox4.Location = new System.Drawing.Point(434, 75);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(52, 24);
+            this.comboBox4.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(223, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(223, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(504, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(504, 77);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(180, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 512);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ApplyFilter);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.textBoxMain);
+            this.Controls.Add(this.openFile);
+            this.Name = "Form1";
+            this.Text = "Фильтрация текста";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        public System.Windows.Forms.TextBox textBoxMain;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.Button ApplyFilter;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Test app/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_app
+{
+    public partial class Form1 : Form
+    {
+        public string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 1;
+            comboBox4.SelectedIndex = 1;
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = openFileDialog1.FileName;
+                textBoxMain.Clear();
+                textBoxMain.Text = File.ReadAllText(name, Encoding.Default);
+            }
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = name;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = saveFileDialog1.FileName;
+                File.WriteAllText(name, textBoxMain.Text);
+            }
+        }
+
+        private void ApplyFilter_Click(object sender, EventArgs e)
+        {
+            string[] lines = textBoxMain.Text.Split('\n');
+            int len = lines.GetLength(0);
+            List<string> ors = new List<string>();
+            List<string> ands = new List<string>();
+            int sublineIdx;
+
+            textBox1.Text = textBox1.Text.Trim(' ');
+            textBox2.Text = textBox2.Text.Trim(' ');
+            textBox3.Text = textBox3.Text.Trim(' ');
+            textBox4.Text = textBox4.Text.Trim(' ');
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                if (comboBox1.SelectedIndex == 0)
+                    ands.Add(textBox1.Text);
+                else
+                    ors.Add(textBox1.Text);
+            }
+            if (!String.IsNullOrEmpty(textBox2.Text))
+            {
+                if (comboBox2.SelectedIndex == 0)
+                    ands.Add(textBox2.Text);
+                else
+                    ors.Add(textBox2.Text);
+            }
+            if (!String.IsNullOrEmpty(textBox3.Text))
+            {
+                if (comboBox3.SelectedIndex == 0)
+                    ands.Add(textBox3.Text);
+                else
+                    ors.Add(textBox3.Text);
+            }
+            if (!String.IsNullOrEmpty(textBox4.Text))
+            {
+                if (comboBox4.SelectedIndex == 0)
+                    ands.Add(textBox4.Text);
+                else
+                    ors.Add(textBox4.Text);
+            }
+            textBoxMain.Clear();
+            for (int i = 0; i < len; ++i)
+            {
+                sublineIdx = -1;
+                foreach (string ptr in ors)
+                {
+                    sublineIdx = lines[i].IndexOf(ptr, StringComparison.CurrentCultureIgnoreCase);
+                    if (sublineIdx != -1)
+                    {
+                        textBoxMain.Text = String.Concat(textBoxMain.Text, lines[i], "\n");
+                        break ;
+                    }
+                }
+                if (sublineIdx == -1)
+                {
+                    foreach (string ptr in ands)
+                    {
+                        sublineIdx = lines[i].IndexOf(ptr, StringComparison.CurrentCultureIgnoreCase);
+                        if (sublineIdx == -1)
+                            break ;
+                    }
+                    if (sublineIdx != -1)
+                        textBoxMain.Text = String.Concat(textBoxMain.Text, lines[i], "\n");
+                }
+            }
+            GC.Collect();
+        }
+
+
+        private void textBoxMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (fileName.Length == 1 && fileName[0].EndsWith(".txt"))
+                    e.Effect = DragDropEffects.Copy;
+                else
+                    e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void textBoxMain_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop);
+                textBoxMain.Clear();
+                textBoxMain.Text = File.ReadAllText(fileName[0], Encoding.Default);
+            }
+        }
+    }
+}
